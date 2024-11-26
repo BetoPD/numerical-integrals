0fd684454d4d49f3b3aa91cd4541fee1563bb37d
Humberto <humberto.angel.duran@gmail.com>
2024-11-26 12:17:43 -0600

    Disable redux dev tools in production

--- src/app/store.js
+++ src/app/store.js
@@ -4,5 +4,6 @@ import paramsReducer from '../features/functionParams/functionParams';
 export default configureStore({
   reducer: {
     params: paramsReducer,
+    devTools: process.env.NODE_ENV !== 'production',
   },
 });
